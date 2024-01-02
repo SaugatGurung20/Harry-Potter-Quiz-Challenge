0edf3d0db8e46e1a5e73645db4f6aa0eab9d5d81
Saugat Gurung <grgseol2015@gmail.com>
2024-01-02 12:44:38 +0000

    added quesitons in questions.js

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+  {
+    title: "What is the core of Harry Potter's wand?",
+    choices: ["Phoenix feather", "Dragon heartstring", "Veela hair", "Thestral tail hair"],
+    correctAnswer: 0
+  },
+  {
+    title: "Who was the Minister of Magic during the events of 'Harry Potter and the Deathly Hallows'?",
+    choices: ["Cornelius Fudge", "Kingsley Shacklebolt", "Rufus Scrimgeour", "Pius Thicknesse"],
+    correctAnswer: 3
+  },
+  {
+    title: "In 'Harry Potter and the Half-Blood Prince', what potion does Professor Slughorn invite Harry to make?",
+    choices: ["Felix Felicis", "Polyjuice Potion", "Amortentia", "Draught of Living Death"],
+    correctAnswer: 2
+  },
+  {
+    title: "What is the name of the mysterious archway in the Department of Mysteries at the Ministry of Magic?",
+    choices: ["The Veil", "The Time Turner", "The Mirror of Erised", "The Pensieve"],
+    correctAnswer: 0
+  },
+  {
+    title: "What magical creature did Luna Lovegood believe in that could only be seen by those who've seen death?",
+    choices: ["Thestral", "Blast-Ended Skrewt", "Niffler", "Hippogriff"],
+    correctAnswer: 0
+  },
+  {
+    title: "What magical object allows Apparition in the wizarding world?",
+    choices: ["Floo Powder", "Portkey", "Apparition Key", "Side-Along-Apparition Charm"],
+    correctAnswer: 1
+  }
+];
